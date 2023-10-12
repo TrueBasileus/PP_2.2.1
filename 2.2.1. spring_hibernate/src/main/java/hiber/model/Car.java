761e4f0d95624cd7550174abe0947544a0b5f084
PIFPAF <yagafarovarclan@gmail.com>
2023-10-12 15:14:36 +0300

    First try

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -0,0 +1,35 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="car")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int series;
+
+    private String model;
+
+    public Car() {}
+
+    public Car(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
